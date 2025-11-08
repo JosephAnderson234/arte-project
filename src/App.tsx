137a383e97208e36cb92668d974bb95a3881272b
JosephAnderson234 <joseph.cose@utec.edu.pe>
2025-11-07 22:05:01 -0500

    feat: routing added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { Outlet } from "react-router"
 
 function App() {
 
   return (
     <div className="w-full h-[100dvh]">
+      <Outlet />
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '@styles/index.css'
-import App from './App'
+import { RouterProvider } from 'react-router'
+import ControlContextProvider from './context/ControlContexProvider'
+import { routes } from './router/routes'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ControlContextProvider>
+      <RouterProvider router={routes}/>
+    </ControlContextProvider>
   </StrictMode>,
 )
